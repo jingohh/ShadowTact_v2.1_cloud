1cfcd3f0db71f9e4935be7f43a7c24e4688dea35
jingohh <love5566.341@gmail.com>
2025-04-03 20:04:37 +0800

    Delete ma_check.py

--- ma_check.py
+++
@@ -1,8 +0,0 @@
-def check_ma():
-    current_ma = 102
-    previous_ma = 100
-    if current_ma > previous_ma:
-        return "均變強"
-    elif current_ma < previous_ma:
-        return "均變弱"
-    return None
